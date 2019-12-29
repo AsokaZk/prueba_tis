29ba047d2870e0e0ca5caddd1251c4086be706fc
AsokaZk <ashok_miau@hotmail.com>
2019-12-29 12:28:30 -0400

    env

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hello Team!
                 </div>
 
                 <div class="links">
